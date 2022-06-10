112ac458d53393f5d65aaabf87c6246c836469eb
abdelrahmanyassero <abdelrahman117ma@gmail.com>
2022-06-10 06:02:47 +0200

    last

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:profile/screens/profile1.dart';
+import 'package:profile/screens/profile10.dart';
 import 'package:profile/screens/profile11.dart';
 import 'package:profile/screens/profile12.dart';
 import 'package:profile/screens/profile13.dart';
 import 'package:profile/screens/profile14.dart';
 import 'package:profile/screens/profile15.dart';
 import 'package:profile/screens/profile16.dart';
+//
+import 'package:profile/screens/profile9.dart';
 void main(){
   runApp(const MyApp());
 }
@@ -13,9 +17,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   MaterialApp(
+    return const  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Profile16(),
+      home: Profile1(),
     );
   }
 }
